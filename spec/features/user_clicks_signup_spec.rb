291043471368b8232cc8e1c1bde602c5c4eff933
Myo T Kyaw <myotkyaw@gmail.com>
2015-09-15 16:04:29 -0700

    Install Devise and test Sign Up link

--- spec/features/user_clicks_signup_spec.rb
+++ spec/features/user_clicks_signup_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-feature "" do
-  scenario "" do
+feature "user clicks on Sign Up link" do
+  scenario "successfully" do
     visit root_path
     click_link ("Sign Up")
-    expect(page).to have_css "h1", text: "Sign Up"
+    expect(page).to have_css 'h2', text: 'Sign Up'
   end
 end
